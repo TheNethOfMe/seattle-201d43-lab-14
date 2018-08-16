3dfd2c5637a3440650aea158eb2d770238b7c28d
Allisa LeBeuf <al5906@gmail.com>
2018-08-16 14:15:50 -0700

    added TODOs for app.js

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -8,15 +8,28 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  var itemToAdd = new CartItem(product, quantity);
+  if (!Cart.items) {
+    Cart.items = [];
+  }
+  Cart.items.push(itemToAdd);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('cartContents', JSON.stringify(Cart.items));
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  var indexToRemove;
+  Cart.items.find(function(cartItem, i) {
+    if (cartItem.product === item) {
+      indexToRemove = i;
+    }
+  });
+  Cart.items.splice(indexToRemove, 1);
 };
 
 var CartItem = function(product, quantity) {
